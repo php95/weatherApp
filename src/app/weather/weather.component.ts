1161b143829250f51ea3b803277a30d0bc5994a5
php95 <mohamedahmedfathy1995@gmail.com>
2023-05-02 15:23:27 +0300

    ui of weather

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent {
+  weatherData = [
+    {iconSrc:"../../assets/min-temp.png",tempType:"min",value:"12"},
+    {iconSrc:"../../assets/max-temp.png",tempType:"max",value:"20"},
+    {iconSrc:"../../assets/humidity.png",tempType:"min",value:"15%"},
+    {iconSrc:"../../assets/wind.png",tempType:"wind",value:"23 km/h"}
+  ]
 
 }
