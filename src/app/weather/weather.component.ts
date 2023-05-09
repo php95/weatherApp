1ae891d66fb34479b667b49b0add09b0502ca31f
php95 <mohamedahmedfathy1995@gmail.com>
2023-05-09 13:56:52 +0300

    finshing weather ui

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
 })
-export class WeatherComponent {
+export class WeatherComponent{
   weatherData = [
     {iconSrc:"../../assets/min-temp.png",tempType:"min",value:"12"},
     {iconSrc:"../../assets/max-temp.png",tempType:"max",value:"20"},
     {iconSrc:"../../assets/humidity.png",tempType:"min",value:"15%"},
-    {iconSrc:"../../assets/wind.png",tempType:"wind",value:"23 km/h"}
+    {iconSrc:"../../assets/wind.png",tempType:"wind",value:"23 "}
   ]
 
 }
